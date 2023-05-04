f3724cc6b77fb04ad32cfe9171838fcc1111f200
LRoslova <larisa1220@mail.ru>
2023-05-04 17:43:12 +0300

    fix

---  browser_extension/analysis.js
+++  browser_extension/analysis.js
@@ -11,9 +11,8 @@ analysBtn.addEventListener("click",() => {
         }
     })
 })
-
 /**
- * Выполняет функцию analysText() на веб-странице указанной
+ * Выполняет функцию grabImages() на веб-странице указанной
  * вкладки и во всех ее фреймах,
  * @param tab {Tab} Объект вкладки браузера
  */
@@ -23,22 +22,118 @@ function execScript(tab) {
     chrome.scripting.executeScript(
         {
             target:{tabId: tab.id, allFrames: true},
-            func:analysText
+            func:grabImages
         },
-        // onResult
+        onResult
     )
 }
 
 /**
- * Функция исполняется на удаленной странице браузера,
- * получает список изображений и возвращает массив
- * путей к ним
+ * Получает список абсолютных путей всех картинок
+ * на удаленной странице
  * 
- *  @return Array массив строк
+ *  @return Array Массив URL
  */
-function analysText() {
-    // стягиваем весь текст с открытой страницы браузера
-    let text= document.documentElement.innerText;
-    console.log(text);
-    return text;    
+function grabImages() {
+    const images = document.querySelectorAll("img");
+    let tmp = Array.from(images).map(image=>image.src);
+    // let height = Array.from(images).map(image=>image.height);
+    // let width = Array.from(images).map(image=>{height: image.height, width: image.width});
+    console.log(tmp);
+    // console.log(height[0]);
+    // console.log(width[0]);
+    return tmp
+      
 }
+let img_height = 180
+let img_width = 180
+const IMAGE_SIZE = 180;
+
+// The minimum image size to consider classifying.  Below this limit the
+// extension will refuse to classify the image.
+const MIN_IMG_SIZE = 128;
+/**
+ * Выполняется после того как вызовы grabImages 
+ * выполнены во всех фреймах удаленной web-страницы.
+ * Функция объединяет результаты в строку и копирует  
+ * список путей к изображениям в буфер обмена
+ * 
+ * @param {[]InjectionResult} frames Массив результатов
+ * функции grabImages
+ */
+function onResult(frames) {
+    // Если результатов нет
+    // if (!frames || !frames.length) { 
+    //     alert("Could not retrieve images from specified page");
+    //     return;
+    // }
+    // // Объединить списки URL из каждого фрейма в один массив
+    // const imageUrls = frames.map(frame=>frame.result)
+    //                         .reduce((r1,r2)=>r1.concat(r2));
+    // // Скопировать в буфер обмена полученный массив  
+    // // объединив его в строку, используя возврат каретки 
+    // // как разделитель  
+    // console.log('hi');
+    // window.navigator.clipboard
+    //       .writeText(imageUrls.join("\n"))
+    //       .then(()=>{
+    //          // закрыть окно расширения после 
+    //          // завершения
+    //          window.close();
+    //       });
+    let src = 'https://habrastorage.org/r/w1560/webt/vt/sm/-2/vtsm-2o5y-t3vhqx5-qaomhxnfw.jpeg'
+    let response;
+    const img = new Image();
+    img.src = src;
+    // return response
+    img.crossOrigin = 'anonymous';
+    img.onload = function(e) {
+      if ((img.height && img.height > MIN_IMG_SIZE) ||
+          (img.width && img.width > MIN_IMG_SIZE)) {
+        img.width = IMAGE_SIZE;
+        img.height = IMAGE_SIZE;
+        // When image is loaded, render it to a canvas and send its ImageData back
+        // to the service worker.
+        const canvas = new OffscreenCanvas(img.width, img.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, img.width, img.height);
+        response = {
+          rawImageData: Array.from(imageData.data),
+          width: img.width,
+          height: img.height,
+        };
+        // return response;
+      }
+      // Fail out if either dimension is less than MIN_IMG_SIZE.
+      alert(`Image size too small. [${img.height} x ${
+          img.width}] vs. minimum [${MIN_IMG_SIZE} x ${MIN_IMG_SIZE}]`);
+        response = {rawImageData: undefined};
+    };
+    img.onerror = function(e) {
+      alert(`Could not load image from external source ${src}.`);
+      response = {rawImageData: undefined};
+    };
+    
+
+    let xhr = new XMLHttpRequest();
+    let json = JSON.stringify({
+    data: response
+    });
+    xhr.open("POST", 'http://localhost:3000/')
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(json)
+    // тело ответа {"сообщение": "Привет, мир!"}
+    xhr.onload = function() {
+        let responseObj = xhr.response;
+        alert(responseObj);
+        // console.log(responseObj);
+        // console.log(responseObj.msg); // Привет, мир!
+    };
+}
+
+function loadImageAndSendDataBack(src) {
+    // Load image (with crossOrigin set to anonymouse so that it can be used in a
+    // canvas later).
+    
+  }
--- server/app.js
+++ server/app.js
@@ -1,31 +1,64 @@
 const tf = require('@tensorflow/tfjs');
-
+// import * as tf from '@tensorflow/tfjs';
 // подключение express
 const express = require("express");
+const bodyParser = require('body-parser');
+canvas = require('canvas')
+
+
 // создаем объект приложения
 const app = express();
+const port = 3000;
+let class_names = {
+    0: "alcohol",
+    1: "drugs",
+    2: "ordinary",
+    3: "porn",
+    4: "weapon"
+}
+let img_height = 180
+let img_width = 180
+const IMAGE_SIZE = 180;
+
+// The minimum image size to consider classifying.  Below this limit the
+// extension will refuse to classify the image.
+const MIN_IMG_SIZE = 128;
+
+
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true })); 
+var cors = require('cors')
+app.use(cors()) // Use this after the variable declaration
+const model = tf.loadLayersModel('/models/5category_180MNV2/model.json');
+
+
+
 // определяем обработчик для маршрута "/"
 app.get("/", function(request, response){
-    // Define a model for linear regression.
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-    // Generate some synthetic data for training.
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-    let tmp;
-    // Train the model using the data.
-    model.fit(xs, ys, {epochs: 10}).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-        tmp = model.predict(tf.tensor2d([5], [1, 1]));
-        model.predict(tf.tensor2d([5], [1, 1])).print();
-    // Open the browser devtools to see the output
-    });
-     
-    // отправляем ответ
-    response.send(`<h2>Привет Express!</h2>`);
+    
+    response.send({msg: "rabotaet!"});
 });
+
+app.post("/", function(request, response){
+    console.log(request.body.data);
+    // let src = request.body.data[0].result[0]
+    let src = 'https://s15.stc.all.kpcdn.net/family/wp-content/uploads/2023/02/top-v-luchshie-porody-krupnykh-sobak-960x540-1-560x420.jpg'
+    console.log(src);
+
+    
+
+    
+
+
+
+
+    response.send({msg: "rabotaet!"});
+});
+
 // начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+// app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+
+
